b452630e186981870956700a4d06403a8a17d643
John Byonghun Kwak <johnkwak1991@yahoo.com>
2016-01-07 12:28:09 -0800

    Stars working

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -69,7 +69,7 @@ end
 
 # ratyrate model.rb
 
-extend ActiveSupport::Concern
+# extend ActiveSupport::Concern
 
   def rate(stars, user, dimension=nil, dirichlet_method=false)
     dimension = nil if dimension.blank?
@@ -181,32 +181,30 @@ extend ActiveSupport::Concern
     dimension ? self.send("#{dimension}_raters") : raters_without_dimension
   end
 
-  module ClassMethods
+  def ratyrate_rater
+    has_many :ratings_given, class_name: 'Rate', foreign_key: :rater_id
+  end
 
-    def ratyrate_rater
-      has_many :ratings_given, class_name: 'Rate', foreign_key: :rater_id
-    end
+  def ratyrate_rateable(*dimensions)
+    has_many :rates_without_dimension, -> { where dimension: nil}, as: :rateable, class_name: 'Rate', dependent: :destroy
+    has_many :raters_without_dimension, through: :rates_without_dimension, source: :rater
 
-    def ratyrate_rateable(*dimensions)
-      has_many :rates_without_dimension, -> { where dimension: nil}, as: :rateable, class_name: 'Rate', dependent: :destroy
-      has_many :raters_without_dimension, through: :rates_without_dimension, source: :rater
+    has_one :rate_average_without_dimension, -> { where dimension: nil}, as: :cacheable,
+            class_name: 'RatingCache', dependent: :destroy
 
-      has_one :rate_average_without_dimension, -> { where dimension: nil}, as: :cacheable,
-              class_name: 'RatingCache', dependent: :destroy
+    dimensions.each do |dimension|
+      has_many "#{dimension}_rates".to_sym, -> {where dimension: dimension.to_s},
+                                            dependent: :destroy,
+                                            class_name: 'Rate',
+                                            as: :rateable
 
-      dimensions.each do |dimension|
-        has_many "#{dimension}_rates".to_sym, -> {where dimension: dimension.to_s},
-                                              dependent: :destroy,
-                                              class_name: 'Rate',
-                                              as: :rateable
+      has_many "#{dimension}_raters".to_sym, through: :"#{dimension}_rates", source: :rater
 
-        has_many "#{dimension}_raters".to_sym, through: :"#{dimension}_rates", source: :rater
-
-        has_one "#{dimension}_average".to_sym, -> { where dimension: dimension.to_s },
-                                              as: :cacheable,
-                                              class_name: 'RatingCache',
+      has_one "#{dimension}_average".to_sym, -> { where dimension: dimension.to_s },
+                                            as: :cacheable,
+                                            class_name: 'RatingCache',
                                               dependent: :destroy
-      end
+      # end
     end
   end
 
@@ -223,13 +221,13 @@ def rating_for(rateable_obj, dimension=nil, options={})
     star_path    = options[:star_path]    || ''
     star_on      = options[:star_on]      || image_path('star-on.png')
     star_off     = options[:star_off]     || image_path('star-off.png')
-    star_half    = options[:star_half]    || image_path('star-half.png')
+    # star_half    = options[:star_half]    || image_path('star-half.png')
     cancel       = options[:cancel]       || false
     cancel_place = options[:cancel_place] || 'left'
     cancel_hint  = options[:cancel_hint]  || 'Cancel current rating!'
     cancel_on    = options[:cancel_on]    || image_path('cancel-on.png')
     cancel_off   = options[:cancel_off]   || image_path('cancel-off.png')
-    noRatedMsg   = options[:noRatedMsg]   || 'I\'am readOnly and I haven\'t rated yet!'
+    # noRatedMsg   = options[:noRatedMsg]   || 'I\'am readOnly and I haven\'t rated yet!'
     # round        = options[:round]        || { down: .26, full: .6, up: .76 }
     space        = options[:space]        || false
     single       = options[:single]       || false
@@ -242,53 +240,54 @@ def rating_for(rateable_obj, dimension=nil, options={})
 
     disable_after_rate = options[:disable_after_rate] && true
     disable_after_rate = true if disable_after_rate == nil
-
-    unless readOnly
-      if disable_after_rate
-        readOnly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
-      else
-        readOnly = !current_user || false
-      end
-    end
-
-    if options[:imdb_avg] && readOnly
-      content_tag :div, '', :style => "background-image:url('#{image_path('mid-star.png')}');width:61px;height:57px;margin-top:10px;" do
-          content_tag :p, avg, :style => "position:relative;font-size:.8rem;text-align:center;line-height:60px;"
-      end
-    else
-      content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
-                  "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name == rateable_obj.class.base_class.name ? rateable_obj.class.name : rateable_obj.class.base_class.name,
-                  "data-disable-after-rate" => disable_after_rate,
-                  "data-readonly" => readOnly,
-                  "data-enable-half" => enable_half,
-                  "data-half-show" => half_show,
-                  "data-star-count" => star,
-                  "data-star-path" => star_path,
-                  "data-star-on" => star_on,
-                  "data-star-off" => star_off,
-                  "data-star-half" => star_half,
-                  "data-cancel" => cancel,
-                  "data-cancel-place" => cancel_place,
-                  "data-cancel-hint"  => cancel_hint,
-                  "data-cancel-on" => cancel_on,
-                  "data-cancel-off" => cancel_off,
-                  "data-no-rated-message" => noRatedMsg,
-                  # "data-round" => round,
-                  "data-space" => space,
-                  "data-single" => single,
-                  "data-target" => target,
-                  "data-target-text" => targetText,
-                  "data-target-type" => targetType,
-                  "data-target-format" => targetFormat,
-                  "data-target-score" => targetScore
-    end
   end
+
+  #   unless readOnly
+  #     if disable_after_rate
+  #       readOnly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
+  #     else
+  #       readOnly = !current_user || false
+  #     end
+  #   end
+
+  #   if options[:imdb_avg] && readOnly
+  #     content_tag :div, '', :style => "background-image:url('#{image_path('mid-star.png')}');width:61px;height:57px;margin-top:10px;" do
+  #         content_tag :p, avg, :style => "position:relative;font-size:.8rem;text-align:center;line-height:60px;"
+  #     end
+  #   else
+  #     content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
+  #                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name == rateable_obj.class.base_class.name ? rateable_obj.class.name : rateable_obj.class.base_class.name,
+  #                 "data-disable-after-rate" => disable_after_rate,
+  #                 "data-readonly" => readOnly,
+  #                 "data-enable-half" => enable_half,
+  #                 "data-half-show" => half_show,
+  #                 "data-star-count" => star,
+  #                 "data-star-path" => star_path,
+  #                 "data-star-on" => star_on,
+  #                 "data-star-off" => star_off,
+  #                 "data-star-half" => star_half,
+  #                 "data-cancel" => cancel,
+  #                 "data-cancel-place" => cancel_place,
+  #                 "data-cancel-hint"  => cancel_hint,
+  #                 "data-cancel-on" => cancel_on,
+  #                 "data-cancel-off" => cancel_off,
+  #                 "data-no-rated-message" => noRatedMsg,
+  #                 # "data-round" => round,
+  #                 "data-space" => space,
+  #                 "data-single" => single,
+  #                 "data-target" => target,
+  #                 "data-target-text" => targetText,
+  #                 "data-target-type" => targetType,
+  #                 "data-target-format" => targetFormat,
+  #                 "data-target-score" => targetScore
+  #   # end
+  # end
 
   def imdb_style_rating_for(rateable_obj, user, options = {})
     #TODO: add option to change the star icon
     overall_avg = rateable_obj.overall_avg(user)
 
-    content_tag :div, '', :style => "background-image:url('#{image_path('big-star.png')}');width:81px;height:81px;margin-top:10px;" do
+    content_tag :div, '', :style => "background-image:url('#{image_path('big-star.png')}');width:70px;height:70px;margin-top:10px;" do
         content_tag :p, overall_avg, :style => "position:relative;line-height:85px;text-align:center;"
     end
   end
@@ -300,8 +299,8 @@ def rating_for(rateable_obj, dimension=nil, options={})
     stars = @rating ? @rating.stars : 0
 
     star         = options[:star]         || 5
-    enable_half  = options[:enable_half]  || false
-    half_show    = options[:half_show]    || true
+    # enable_half  = options[:enable_half]  || false
+    # half_show    = options[:half_show]    || true
     star_path    = options[:star_path]    || ''
     star_on      = options[:star_on]      || image_path('star-on.png')
     star_off     = options[:star_off]     || image_path('star-off.png')
